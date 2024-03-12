5e2bcc6c5e0b31df0628406772b7af717905522a
Mayra Capeletti <mayracapeletti@hotmail.com>
2024-03-12 17:32:13 -0300

    modificar y añadir elementos a la tabla

--- mipasteleria/models/novedadesModel.js
+++ mipasteleria/models/novedadesModel.js
@@ -13,4 +13,31 @@ async function getNovedades(){
         return rows;
     }    
 
-module.exports = { getNovedades, deleteNovedadesById }
+    async function insertNovedad(obj){
+        try{
+            var query = "insert into recetas set ?";
+            var rows = await pool.query(query, [obj])
+            return rows;
+        }catch (error) {
+            console.log (error);
+            throw error;
+        }
+    }
+
+    async function getNovedadById(id){
+        var query = "select * from recetas where id =?"
+        var rows = await pool.query(query, [id]);
+        return rows [0];
+    }
+
+    async function modificarNovedadById (obj, id){
+        try {
+            var query = "update recetas set ? where id=? ";
+            var rows = await pool.query(query, [obj, id]);
+            return rows;
+        }catch(error){
+            throw error;
+        }
+    }
+
+module.exports = { getNovedades, deleteNovedadesById, insertNovedad, getNovedadById, modificarNovedadById}
